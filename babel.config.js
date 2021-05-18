862b718d6baa2aeb6d9a8936d86782a20053c468
goran <goran.novacic@gmail.com>
2021-05-18 20:38:53 +0200

    first commit

--- babel.config.js
+++ babel.config.js
@@ -41,12 +41,7 @@ module.exports = function(api) {
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
+      '@babel/plugin-proposal-class-properties',
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
@@ -56,9 +51,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false,
-          regenerator: true,
-          corejs: false
+          helpers: false
         }
       ],
       [
