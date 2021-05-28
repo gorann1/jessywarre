b235f35158f5ef1348791bcf6c0e73179faed87a
goran <goran.novacic@gmail.com>
2021-05-29 01:49:34 +0200

    test_nginx#10

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    Rails.application.config.hosts << "scubabooker.eu"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
