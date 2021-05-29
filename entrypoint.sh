1d24b0f2b1b29ff80000a7745e2b8d02eaa8a77c
goran <goran.novacic@gmail.com>
2021-05-29 14:02:23 +0200

    test_nginx#21

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,6 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# Allow nginx to stay in the foreground
-# so that Docker can track the process properly
-nginx -g 'daemon off;'
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
 
